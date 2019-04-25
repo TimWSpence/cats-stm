6afcf22aec245debbfe327e02fb20240a6adc74b
Tim Spence <timothywspence@gmail.com>
2019-04-25 18:23:58 +0100

    Add concurrent transactions test
    - We ran a randomly generated set of transactions on a randomly
      generated list of TVar[Long] and ensure that the total of the
      values of the tvars is invariant under the execution of the
      transactions

---
+++ src/test/scala/org/timwspence/cats/stm/PropertyTests.scala
@@ -0,0 +1,63 @@
+package org.timwspence.cats.stm
+
+import java.util.concurrent.atomic.AtomicReference
+
+import cats.effect.{ContextShift, IO, Timer}
+import cats.instances.list._
+import cats.syntax.functor._
+import cats.syntax.traverse._
+import org.scalacheck.Prop.forAll
+import org.scalacheck._
+
+import scala.concurrent.ExecutionContext.Implicits
+import scala.util.Random
+
+/**
+  * Test that concurrently executing transactions behave correctly.
+  * We do this by setting up a list of TVar[Long] and a set of
+  * STM transactions which subtracts an amount from one tvar and
+  * adds the same amount to another tvar. The sum of the tvar values
+  * should be invariant under the execution of all these transactions.
+  */
+object MaintainsInvariants extends Properties("STM") {
+  val executionContext = Implicits.global
+
+  implicit val timer: Timer[IO] = IO.timer(executionContext)
+
+  implicit val cs: ContextShift[IO] = IO.contextShift(executionContext)
+
+  val tvarGen: Gen[TVar[Long]] = for {
+    id    <- Gen.posNum[Long]
+    value <- Gen.posNum[Long]
+  } yield new TVar(id, value, new AtomicReference(Map()))
+
+  val txnGen: List[TVar[Long]] => Gen[STM[Unit]] = tvars => for {
+    fromIdx <- Gen.choose(0, tvars.length - 1)
+    toIdx   <- Gen.choose(0, tvars.length - 1) suchThat (_ != fromIdx)
+    txn     <- for {
+      balance <- tvars(fromIdx).get
+      transfer = Math.abs(Random.nextLong()) % balance
+      _       <- tvars(fromIdx).modify(_ - transfer)
+      _       <- tvars(toIdx).modify(_ + transfer)
+    } yield ()
+  } yield txn
+
+  val gen: Gen[(Long, List[TVar[Long]], IO[Unit])] = for {
+    tvars <- Gen.listOfN(50, tvarGen)
+    total  = tvars.foldLeft(0L)((acc, tvar) => acc + tvar.value)
+    txns  <- Gen.listOf(txnGen(tvars))
+    commit = txns.traverse(_.commit[IO].start)
+    run    = commit.flatMap(l => l.traverse(_.join)).void
+  } yield (total, tvars, run)
+
+  property("Transactions maintain total") = forAll(gen) { g =>
+    val total = g._1
+    val tvars = g._2
+    val txn   = g._3
+
+    txn.unsafeRunSync
+
+    tvars.map(_.value).sum == total
+  }
+
+}
