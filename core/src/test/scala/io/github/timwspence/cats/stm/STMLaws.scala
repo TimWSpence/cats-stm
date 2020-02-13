b5e8c99cda79b65efb25df95312a373aa81f945d
Tim Spence <timothywspence@gmail.com>
2020-02-13 11:37:35 +0000

    Wip

--- core/src/test/scala/io/github/timwspence/cats/stm/STMLaws.scala
+++ core/src/test/scala/io/github/timwspence/cats/stm/STMLaws.scala
@@ -13,18 +13,34 @@ import cats.kernel.laws.discipline._
 import cats.laws.discipline.eq._
 import cats.laws.discipline.arbitrary._
 
+import shapeless._
+
 import Helpers._
 import org.scalacheck.Arbitrary
 import io.github.timwspence.cats.stm.STM.internal.TLog
 import io.github.timwspence.cats.stm.STM.internal.TResult
+import io.github.timwspence.cats.stm.STM.internal.TSuccess
+import io.github.timwspence.cats.stm.STM.internal.TRetry
+import io.github.timwspence.cats.stm.STM.internal.TFailure
 
 object Helpers {
-  implicit def eqTResult[A: Eq : shapeless.Typeable]: Eq[TResult[A]] = {
-    // import auto.eq._
-    semi.eq[TResult[A]]
+  implicit def eqTResult[A](implicit A: Eq[A]): Eq[TResult[A]] = new Eq[TResult[A]] {
+
+    override def eqv(x: TResult[A], y: TResult[A]): Boolean = (x,y) match {
+      case (TSuccess(a1), TSuccess(a2)) => A.eqv(a1,a2)
+      case (TRetry, TRetry) => true
+      case (TFailure(_), TFailure(_)) => true // This is a bit dubious but we don't have an
+                                              // Eq instance for Throwable so ¯\_(ツ)_/¯
+      case _ => false
+    }
+
   }
 
   implicit  def eqSTM[A: Eq](implicit Log: ExhaustiveCheck[TLog]): Eq[STM[A]] = Eq.instance((stm1, stm2) => Log.allValues.forall(a => Eq[TResult[A]].eqv(stm1.run(a), stm1.run(a))))
+
+
+  //Possibly the best way to build a stm is to generate list of tvars and then from that generate list of stm actions from that
+  //and fold with bind
 
   implicit val arbitraryTLog: Arbitrary[TLog] = ???
 
