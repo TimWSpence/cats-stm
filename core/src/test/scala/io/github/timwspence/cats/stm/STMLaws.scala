9032bcf52106aa431bd7960b066f200e22ad14c9
Tim Spence <timothywspence@gmail.com>
2020-02-13 17:20:05 +0000

    Fix monad laws

--- core/src/test/scala/io/github/timwspence/cats/stm/STMLaws.scala
+++ core/src/test/scala/io/github/timwspence/cats/stm/STMLaws.scala
@@ -18,7 +18,7 @@ import cats.laws.discipline.arbitrary._
 
 import shapeless._
 
-import Helpers._
+import Implicits._
 import org.scalacheck.{Arbitrary, Gen}
 import io.github.timwspence.cats.stm.STM.internal.TLog
 import io.github.timwspence.cats.stm.STM.internal.TResult
@@ -27,41 +27,45 @@ import io.github.timwspence.cats.stm.STM.internal.TRetry
 import io.github.timwspence.cats.stm.STM.internal.TFailure
 import org.typelevel.discipline.scalatest.FunSuiteDiscipline
 
-object Helpers {
+object Implicits extends LowPriorityImplicits {
   implicit def eqTResult[A](implicit A: Eq[A]): Eq[TResult[A]] = new Eq[TResult[A]] {
 
-    override def eqv(x: TResult[A], y: TResult[A]): Boolean = (x,y) match {
-      case (TSuccess(a1), TSuccess(a2)) => A.eqv(a1,a2)
-      case (TRetry, TRetry) => true
-      case (TFailure(_), TFailure(_)) => true // This is a bit dubious but we don't have an
-                                              // Eq instance for Throwable so ¯\_(ツ)_/¯
+    override def eqv(x: TResult[A], y: TResult[A]): Boolean = (x, y) match {
+      case (TSuccess(a1), TSuccess(a2)) => A.eqv(a1, a2)
+      case (TRetry, TRetry)             => true
+      case (TFailure(_), TFailure(_))   => true // This is a bit dubious but we don't have an
+      // Eq instance for Throwable so ¯\_(ツ)_/¯
       case _ => false
     }
 
   }
 
-  implicit def genTVar[A](implicit A: Gen[A]): Gen[TVar[A]] = A.map(a => TVar.of(a).commit[IO].unsafeRunSync)
+  implicit def eqSTM[A](implicit A: Eq[A]): Eq[STM[A]] =
+    Eq.instance((stm1, stm2) => stm1.run(TLog.empty) === stm2.run(TLog.empty))
 
-  // Why doesn't this work?
+  // Why doesn't this work? Just use the simple eqSTM above for now. It achieves what is intended anyway
+
   // implicit  def eqSTM[A: Eq](implicit Log: ExhaustiveCheck[TLog]): Eq[STM[A]] = Eq.instance((stm1, stm2) => Log.allValues.forall(a => Eq[TResult[A]].eqv(stm1.run(a), stm1.run(a))))
 
   //This looks a bit weird but STM.run is always invoked with an empty TLog so it's ok
   // implicit val exhaustiveCheckTlog: ExhaustiveCheck[TLog] = ExhaustiveCheck.instance(List(TLog.empty))
 
-  implicit def eqSTM[A](implicit A: Eq[A]): Eq[STM[A]] = Eq.instance((stm1, stm2) => stm1.run(TLog.empty) === stm2.run(TLog.empty))
+  implicit def genTVar[A](implicit A: Gen[A]): Gen[TVar[A]] = A.map(a => TVar.of(a).commit[IO].unsafeRunSync)
 
-
-  implicit def genSTM[A: Gen : Order](implicit TVar: Gen[TVar[A]], M: Monoid[A]): Gen[STM[A]] = {
-    def genGetSTM[A](tv: TVar[A]): Gen[STM[Unit]] = Gen.const(tv.get.void)
+  implicit def genSTM[A: Gen: Order](implicit TVar: Gen[TVar[A]], M: Monoid[A]): Gen[STM[A]] = {
+    def genGetSTM[A](tv: TVar[A]): Gen[STM[Unit]]                     = Gen.const(tv.get.void)
     def genSetSTM[A](tv: TVar[A])(implicit A: Gen[A]): Gen[STM[Unit]] = A.map(tv.set)
-    def genModifySTM[A](tv: TVar[A])(implicit A: Gen[A], M: Monoid[A]): Gen[STM[Unit]] = A.map(a1 => tv.modify(a2 => M.combine(a1, a2)))
-    def genCheckSTM[A](tv: TVar[A])(implicit A: Gen[A], Ord: Order[A]): Gen[STM[Unit]] = A.map(a1 => tv.get >>= (a2 => STM.check(Ord.compare(a1, a2) > 0)))
-    def genSingleSTM[A: Gen : Monoid : Order](tv: TVar[A]): Gen[STM[Unit]] = Gen.oneOf(genGetSTM(tv), genSetSTM(tv), genModifySTM(tv), genCheckSTM(tv))
+    def genModifySTM[A](tv: TVar[A])(implicit A: Gen[A], M: Monoid[A]): Gen[STM[Unit]] =
+      A.map(a1 => tv.modify(a2 => M.combine(a1, a2)))
+    def genCheckSTM[A](tv: TVar[A])(implicit A: Gen[A], Ord: Order[A]): Gen[STM[Unit]] =
+      A.map(a1 => tv.get >>= (a2 => STM.check(Ord.compare(a1, a2) > 0)))
+    def genSingleSTM[A: Gen: Monoid: Order](tv: TVar[A]): Gen[STM[Unit]] =
+      Gen.oneOf(genGetSTM(tv), genSetSTM(tv), genModifySTM(tv), genCheckSTM(tv))
     def sumAll[A](tvs: List[TVar[A]])(implicit M: Monoid[A]): STM[A] = tvs.traverse(_.get).map(M.combineAll)
 
     for {
       tvars <- Gen.listOfN(5, TVar)
-      stms  <- Gen.nonEmptyListOf(for  {
+      stms <- Gen.nonEmptyListOf(for {
         idx <- Gen.choose(0, tvars.length - 1)
         tv = tvars(idx)
         stm <- genSingleSTM(tv)
@@ -69,11 +73,17 @@ object Helpers {
     } yield stms.reduce(_ >> _) >> sumAll(tvars)
   }
 
-  implicit def arbSTM[A: Monoid : Gen : Order](implicit Gen: Gen[STM[A]]): Arbitrary[STM[A]] = Arbitrary(Gen)
+  implicit def arbSTM[A: Monoid: Gen: Order](implicit Gen: Gen[STM[A]]): Arbitrary[STM[A]] = Arbitrary(Gen)
 
-  implicit val genInt: Gen[Int] = Gen.posNum[Int]
+  implicit val genInt: Gen[Int]       = Gen.posNum[Int]
   implicit val genString: Gen[String] = Gen.alphaNumStr
 
+}
+
+trait LowPriorityImplicits {
+  // Generates non-interesting arbitrary STMs using pure
+  // Necessary as the Monad laws require Arbitrary[A => B]
+  implicit def arbSTMNoConstraints[A](implicit A: Arbitrary[A]): Arbitrary[STM[A]] = Arbitrary(A.arbitrary.map(STM.pure))
 }
 
 class STMLaws extends AnyFunSuite with FunSuiteDiscipline with Configuration {
@@ -83,5 +93,5 @@ class STMLaws extends AnyFunSuite with FunSuiteDiscipline with Configuration {
 
   checkAll("STM[Int]", FunctorTests[STM].functor[Int, Int, Int])
 
-  // checkAll("STM[Int]", MonadTests[STM].monad[Int, Int, Int])
+  checkAll("STM[Int]", MonadTests[STM].monad[Int, Int, Int])
 }
