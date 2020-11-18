515702059e5923323d3fa9faef2df5b63c22a0b3
Tim Spence <timothywspence@gmail.com>
2020-11-18 10:47:54 +0000

    Make eval construction of TVars safe without suspending via F.delay (#199)

--- laws/src/test/scala/io/github/timwspence/cats/stm/CatsLawsSpec.scala
+++ laws/src/test/scala/io/github/timwspence/cats/stm/CatsLawsSpec.scala
@@ -22,17 +22,9 @@ import cats.laws.discipline._
 import munit.DisciplineSuite
 
 class CatsLawsSpec extends DisciplineSuite with Instances {
-  checkAll("STM[Int]", SemigroupTests[STM[Int]].semigroup)
-
   checkAll("STM[Int]", MonoidTests[STM[Int]].monoid)
 
-  checkAll("STM[Int]", FunctorTests[STM].functor[Int, Int, Int])
-
-  checkAll("STM[Int]", ApplicativeTests[STM].applicative[Int, Int, Int])
-
   checkAll("STM[Int]", MonadTests[STM].monad[Int, Int, Int])
-
-  checkAll("STM[Int]", SemigroupKTests[STM].semigroupK[Int])
 
   checkAll("STM[Int]", MonoidKTests[STM].monoidK[Int])
 }
