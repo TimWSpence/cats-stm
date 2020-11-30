55ac310f21941b494d54a9db8f13bf5efaa29377
Tim Spence <timothywspence@gmail.com>
2020-11-30 15:21:23 +0000

    Re-enable tvar test

--- core/src/test/scala/io/github/timwspence/cats/stm/TVarTest.scala
+++ core/src/test/scala/io/github/timwspence/cats/stm/TVarTest.scala
@@ -14,179 +14,86 @@
  * limitations under the License.
  */
 
-// /*
-//  * Copyright 2020 TimWSpence
-//  *
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  *
-//  *     http://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
-//  */
+package io.github.timwspence.cats.stm
 
-// package io.github.timwspence.cats.stm
+import scala.concurrent.duration._
 
-// import scala.concurrent.duration._
+import cats.effect.IO
+import munit.CatsEffectSuite
 
-// import cats.effect.IO
-// import munit.CatsEffectSuite
+class TVarTest extends CatsEffectSuite {
 
-// class TVarTest extends CatsEffectSuite {
+  val stm = STM[IO]().unsafeRunSync()
+  import stm._
 
-//   test("Get returns current value") {
-//     val prog: STM[String] = for {
-//       tvar  <- TVar.of("hello")
-//       value <- tvar.get
-//     } yield value
+  test("Get returns current value") {
+    val prog: Txn[String] = for {
+      tvar  <- TVar.of("hello")
+      value <- tvar.get
+    } yield value
 
-//     for (value <- prog.atomically[IO]) yield assertEquals(value, "hello")
-//   }
+    for (value <- stm.commit(prog)) yield assertEquals(value, "hello")
+  }
 
-//   test("Set changes current value") {
-//     val prog: STM[String] = for {
-//       tvar  <- TVar.of("hello")
-//       _     <- tvar.set("world")
-//       value <- tvar.get
-//     } yield value
+  test("Set changes current value") {
+    val prog: Txn[String] = for {
+      tvar  <- TVar.of("hello")
+      _     <- tvar.set("world")
+      value <- tvar.get
+    } yield value
 
-//     for (value <- prog.atomically[IO]) yield assertEquals(value, "world")
-//   }
+    for (value <- stm.commit(prog)) yield assertEquals(value, "world")
+  }
 
-//   test("Modify changes current value") {
-//     val prog: STM[String] = for {
-//       tvar  <- TVar.of("hello")
-//       _     <- tvar.modify(_.toUpperCase)
-//       value <- tvar.get
-//     } yield value
+  test("Modify changes current value") {
+    val prog: Txn[String] = for {
+      tvar  <- TVar.of("hello")
+      _     <- tvar.modify(_.toUpperCase)
+      value <- tvar.get
+    } yield value
 
-//     for (value <- prog.atomically[IO]) yield assertEquals(value, "HELLO")
-//   }
+    for (value <- stm.commit(prog)) yield assertEquals(value, "HELLO")
+  }
 
-//   test("Transaction is registered for retry") {
-//     val tvar = TVar.of(0).atomically[IO].unsafeRunSync()
+  test("Transaction is registered for retry") {
+    val tvar = stm.commit(TVar.of(0)).unsafeRunSync()
 
-//     val prog: IO[(Int, Int)] = for {
-//       fiber <- (for {
-//           current <- tvar.get
-//           _       <- STM.check(current > 0)
-//         } yield current).atomically[IO].start
-//       _ <- IO.sleep(1 second)
-//       pending = tvar.pending.get.size
-//       _   <- tvar.set(2).atomically[IO]
-//       res <- fiber.join
-//     } yield (pending, res)
+    val prog: IO[Int] = for {
+      fiber <-
+        stm
+          .commit((for {
+            current <- tvar.get
+            _       <- stm.check(current > 0)
+          } yield current))
+          .start
+      _   <- IO.sleep(1 second)
+      _   <- stm.commit(tvar.set(2))
+      res <- fiber.joinAndEmbedNever
+    } yield res
 
-//     prog.map { res =>
-//       assertEquals(res._1, 1)
-//       assertEquals(res._2, 2)
-//     }
+    prog.map { res =>
+      assertEquals(res, 2)
+    }
 
-//   }
+  }
 
-//   test("Transaction is re-registered for retry") {
-//     val tvar = TVar.of(0).atomically[IO].unsafeRunSync()
+  test("TVar.of is referentially transparent") {
+    val t: Txn[TVar[Int]] = TVar.of(0)
 
-//     val prog: IO[(Int, Int, Int)] = for {
-//       fiber <- (for {
-//           current <- tvar.get
-//           _       <- STM.check(current > 1)
-//         } yield current).atomically[IO].start
-//       _ <- IO.sleep(1 second)
-//       pending = tvar.pending.get.size
-//       _ <- tvar.set(1).atomically[IO]
-//       _ <- IO.sleep(1 second)
-//       pending2 = tvar.pending.get.size
-//       _ <- tvar.set(2).atomically[IO]
-//       _ <- IO.sleep(1 second)
-//       pending3 = tvar.pending.get.size
-//       _ <- fiber.join
-//     } yield (pending, pending2, pending3)
+    val prog = for {
+      t1 <- stm.commit(t)
+      t2 <- stm.commit(t)
+      _ <- stm.commit((for {
+        _ <- t1.modify(_ + 1)
+        _ <- t2.modify(_ + 2)
+      } yield ()))
+      v1 <- stm.commit(t1.get)
+      v2 <- stm.commit(t2.get)
+    } yield (v1 -> v2)
 
-//     prog.map { res =>
-//       assertEquals(res._1, 1)
-//       assertEquals(res._2, 1)
-//       assertEquals(res._3, 0)
-//     }
-
-//   }
-
-//   test("Retry is unregistered on success") {
-//     val tvar = TVar.of(0).atomically[IO].unsafeRunSync()
-
-//     val prog: IO[(Int, Int)] = for {
-//       fiber <- (for {
-//           current <- tvar.get
-//           _       <- STM.check(current > 0)
-//         } yield current).atomically[IO].start
-//       _ <- IO.sleep(1 second)
-//       pending = tvar.pending.get.size
-//       _ <- tvar.set(2).atomically[IO]
-//       _ <- fiber.join
-//       pending2 = tvar.pending.get.size
-//     } yield (pending, pending2)
-
-//     prog.map { res =>
-//       assertEquals(res._1, 1)
-//       assertEquals(res._2, 0)
-//     }
-
-//   }
-
-//   test("Pending transaction is removed on success") {
-//     val tvar = TVar.of("foo").atomically[IO].unsafeRunSync()
-
-//     val prog: STM[String] = for {
-//       _     <- tvar.modify(_.toUpperCase)
-//       value <- tvar.get
-//     } yield value
-
-//     for (value <- prog.atomically[IO]) yield {
-//       assertEquals(value, "FOO")
-
-//       assertEquals(tvar.value, "FOO")
-//       assert(tvar.pending.get.isEmpty)
-//     }
-//   }
-
-//   test("Pending transaction is removed on failure") {
-//     val tvar = TVar.of("foo").atomically[IO].unsafeRunSync()
-
-//     val prog: STM[String] = for {
-//       _     <- tvar.modify(_.toUpperCase)
-//       _     <- STM.abort[String](new RuntimeException("boom"))
-//       value <- tvar.get
-//     } yield value
-
-//     for (_ <- prog.atomically[IO].attempt) yield {
-//       assertEquals(tvar.value, "foo")
-
-//       assert(tvar.pending.get.isEmpty)
-//     }
-//   }
-
-//   test("TVar.of is referentially transparent") {
-//     val t: STM[TVar[Int]] = TVar.of(0)
-
-//     val prog = for {
-//       t1 <- t.atomically[IO]
-//       t2 <- t.atomically[IO]
-//       _ <- (for {
-//           _ <- t1.modify(_ + 1)
-//           _ <- t2.modify(_ + 2)
-//         } yield ()).atomically[IO]
-//       v1 <- t1.get.atomically[IO]
-//       v2 <- t2.get.atomically[IO]
-//     } yield (v1 -> v2)
-
-//     prog.map { res =>
-//       assertEquals(res._1, 1)
-//       assertEquals(res._2, 2)
-//     }
-//   }
-// }
+    prog.map { res =>
+      assertEquals(res._1, 1)
+      assertEquals(res._2, 2)
+    }
+  }
+}
