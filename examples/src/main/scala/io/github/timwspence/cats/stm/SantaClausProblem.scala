3addc48d3b7bfe13389ab53ac786fedd137ecc7f
Tim Spence <timothywspence@gmail.com>
2020-12-04 17:58:57 +0000

    Real santa claus bug minimization

--- examples/src/main/scala/io/github/timwspence/cats/stm/SantaClausProblem.scala
+++ examples/src/main/scala/io/github/timwspence/cats/stm/SantaClausProblem.scala
@@ -30,111 +30,48 @@ object SantaClausProblem extends IOApp.Simple {
   override def run: IO[Unit] =
     mainProblem.timeout(5.seconds)
 
-  def meetInStudy(id: Int): IO[Unit] = IO.println(show"Elf $id meeting in the study")
-
-  sealed abstract case class Gate(capacity: Int, tv: TVar[Int]) {
-    def pass: IO[Unit]    = Gate.pass(this)
-    def operate: IO[Unit] = Gate.operate(this)
-  }
-  object Gate {
-    def of(capacity: Int) =
-      TVar.of(0).map(new Gate(capacity, _) {})
-
-    def pass(g: Gate): IO[Unit] =
-      stm.commit {
-        for {
-          nLeft <- g.tv.get
-          _     <- stm.check({println(s"nLeft in pass is $nLeft"); nLeft > 0})
-          _     <- g.tv.modify(_ - 1)
-        } yield ()
-      }
-
-    def operate(g: Gate): IO[Unit] =
-      for {
-        _ <- stm.commit(g.tv.set(g.capacity))
-        _ <- stm.commit {
-          for {
-            nLeft <- g.tv.get
-            _     <- stm.check(nLeft === 0)
-          } yield ()
-        }
-      } yield ()
-  }
-
-  sealed abstract case class Group(
-    n: Int,
-    tv: TVar[(Int, Gate, Gate)]
-  ) {
-    def join: IO[(Gate, Gate)]   = Group.join(this)
-    def await: Txn[(Gate, Gate)] = Group.await(this)
-  }
-  object Group {
-    def of(n: Int): IO[Group] =
-      stm.commit {
-        for {
-          g1 <- Gate.of(n)
-          g2 <- Gate.of(n)
-          tv <- TVar.of((n, g1, g2))
-        } yield new Group(n, tv) {}
-      }
-
-    def join(g: Group): IO[(Gate, Gate)] =
-      stm.commit {
-        for {
-          (nLeft, g1, g2) <- g.tv.get
-          _               <- stm.check(nLeft > 0)
-          _               <- g.tv.set((nLeft - 1, g1, g2))
-        } yield (g1, g2)
-      }
-    def await(g: Group): Txn[(Gate, Gate)] =
-      for {
-        (nLeft, g1, g2) <- g.tv.get
-        _               <- {println(s"nLeft is $nLeft"); stm.check(nLeft === 0)}
-        newG1           <- Gate.of(g.n)
-        newG2           <- Gate.of(g.n)
-        _               <- g.tv.set((g.n, newG1, newG2))
-      } yield (g1, g2)
-  }
-
-  def helper1(group: Group, doTask: IO[Unit]): IO[Unit] =
-    (for {
-      _ <- IO.println("Joining gate")
-      (inGate, outGate) <- group.join
-      _                 <- inGate.pass
-      _                 <- doTask
-      _                 <- outGate.pass
-     } yield ()).handleErrorWith {
-      case e => IO(e.printStackTrace())
-    }
-
-  def elf(g: Group, i: Int) =
-    helper1(g, meetInStudy(i)).foreverM.start
-
-  def santa(elfGroup: Group): IO[Unit] = {
-    def run(task: String, gates: (Gate, Gate)): IO[Unit] =
-      for {
-        _ <- IO.println(show"Ho! Ho! Ho! let’s $task")
-        _ <- gates._1.operate
-        _ <- IO.println("operated gate 1") //Seems santa doesn't get this far?
-        _ <- gates._2.operate
-        _ <- IO.println("operated gates") //Seems santa doesn't get this far?
-      } yield ()
-
-    (for {
-      _ <- IO.println("----------")
-      _ <- stm.commit(elfGroup.await).flatMap {
-        g: (Gate, Gate) => run("meet in study", g)
-      }
-     } yield ()).handleErrorWith {
-      case e => IO(e.printStackTrace())
-    }
-  }
-
   def mainProblem: IO[Unit] =
     for {
-      elfGroup <- Group.of(1)
-      _         <- List(1).traverse_(n => elf(elfGroup, n))
-      _         <- santa(elfGroup).foreverM.void
+      in <- stm.commit(TVar.of(0))
+      out <- stm.commit(TVar.of(0))
+      elf = (for {
+        _ <- stm.commit(
+          for {
+            cur <- in.get
+            _ <- stm.check(cur == 1)
+            _ <- in.set(0)
+          } yield ()
+        )
+        _ <- IO.println("elf stuff")
+        _ <- stm.commit(
+          for {
+            cur <- out.get
+            _ <- stm.check(cur == 1)
+            _ <- out.set(0)
+          } yield ()
+        )
+      } yield ()).foreverM
+      santa = (for {
+        _ <- stm.commit(
+          for {
+            cur <- in.get
+            _ <- stm.check(cur == 0)
+            _ <- in.set(1)
+          } yield ()
+        )
+        _ <- IO.println("santa stuff")
+        _ <- stm.commit(
+          for {
+            cur <- out.get
+            _ <- stm.check(cur == 0)
+            _ <- out.set(1)
+          } yield ()
+        )
+      } yield ()).foreverM
+      e <- elf.start
+      s <- santa.start
+      _ <- e.join
+      _ <- s.join
     } yield ()
 
 }
