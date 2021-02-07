5cca6f916e1335faa465b8b499447adfc53c0be0
Jakub Kozłowski <kubukoz@gmail.com>
2021-02-07 20:38:27 +0100

    Use overlays correctly

--- shell.nix
+++ shell.nix
@@ -1,14 +1,15 @@
 # Credit to https://github.com/profunktor/redis4cats/blob/master/shell.nix
 let
-  jdk11 = self: super: rec {
-    jre = pkgs.jdk11;
-  };
-
   nixpkgs = fetchTarball {
     name = "nixpkgs-20-09";
     url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
     sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
   };
+
+  jdk11 = self: _: {
+    jre = self.jdk11;
+  };
+
   pkgs = import nixpkgs { overlays = [ jdk11 ]; };
 in
 pkgs.mkShell {
